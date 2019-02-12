d2ad9457bf413966f9a5b0b1797a48c519a41068
Connor Davis <davisc@ainfosec.com>
2019-02-12 16:02:00 -0700

    Bring in iommu from old hyperkernel
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

---
+++ bfvmm/include/hve/arch/intel_x64/iommu.h
@@ -0,0 +1,140 @@
+//
+// Bareflank Boxy
+// Copyright (C) 2019 Assured Information Security, Inc.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#ifndef IOMMU_INTEL_X64_BOXY_H
+#define IOMMU_INTEL_X64_BOXY_H
+
+#include <cstdint>
+#include <bfvmm/hve/arch/x64/unmapper.h>
+#include <hve/arch/intel_x64/vcpu.h>
+
+// -----------------------------------------------------------------------------
+// Exports
+// -----------------------------------------------------------------------------
+
+#include <bfexports.h>
+
+#ifndef STATIC_BOXY_HVE
+#ifdef SHARED_BOXY_HVE
+#define EXPORT_BOXY_HVE EXPORT_SYM
+#else
+#define EXPORT_BOXY_HVE IMPORT_SYM
+#endif
+#else
+#define EXPORT_BOXY_HVE
+#endif
+
+//------------------------------------------------------------------------------
+// Definition
+//------------------------------------------------------------------------------
+
+namespace vtd {
+    extern uint64_t visr_vector;
+    extern uint64_t ndvm_vector;
+    extern uint64_t ndvm_apic_id;
+    extern uint64_t ndvm_vcpu_id;
+}
+
+namespace boxy::intel_x64 {
+
+class EXPORT_BOXY_HVE iommu
+{
+public:
+
+    using entry_t = struct { uint64_t data[2]; } __attribute__((packed));
+
+    /// This is found in the register base address field
+    /// of DRHD[1] on the gigabyte board. DRHD[0] is at
+    /// the previous page, but only scopes the graphics
+    /// card.
+    ///
+    /// Obviously this will potentially change from system to
+    /// system, so it should be read generically out of the DMAR
+    ///
+    static constexpr uintptr_t hpa = 0xFED91000UL;
+
+    /// Only 4K pages allowed
+    ///
+    static constexpr uintptr_t page_size = 4096UL;
+
+    /// Get the instance of the iommu
+    ///
+    static iommu *instance() noexcept;
+
+    /// Destructor
+    ///
+    /// @expects
+    /// @ensures
+    ///
+    ~iommu() = default;
+
+    void init_dom0_mappings();
+    void init_domU_mappings();
+
+    void set_dom0_eptp(uintptr_t eptp);
+    void set_domU_eptp(uintptr_t eptp);
+
+    void set_dom0_cte(entry_t *cte);
+    void set_domU_cte(entry_t *cte);
+
+    void enable();
+    void disable();
+
+    /// Register access
+    ///
+    uint64_t read64(uintptr_t off);
+    uint32_t read32(uintptr_t off);
+    void write64(uintptr_t off, uint64_t val);
+    void write32(uintptr_t off, uint64_t val);
+
+private:
+
+    iommu() noexcept;
+
+    bfvmm::x64::unique_map<uint8_t> m_reg_map;
+    uint8_t *m_hva{reinterpret_cast<uint8_t *>(hpa)};
+    uintptr_t m_dom0_eptp{0};
+    uintptr_t m_domU_eptp{0};
+
+    page_ptr<entry_t> m_root;
+    std::vector<page_ptr<entry_t>> m_ctxt;
+
+public:
+
+    /// @cond
+
+    iommu(iommu &&) noexcept = delete;
+    iommu &operator=(iommu &&) noexcept = delete;
+
+    iommu(const iommu &) = delete;
+    iommu &operator=(const iommu &) = delete;
+
+    /// @endcond
+};
+}
+
+/// iommu macro
+///
+/// The following macro can be used to quickly call for iommu services
+///
+/// @expects
+/// @ensures g_iommu != nullptr
+///
+#define g_iommu boxy::intel_x64::iommu::instance()
+
+#endif
---
+++ bfvmm/src/hve/arch/intel_x64/iommu.cpp
@@ -0,0 +1,261 @@
+//
+// Bareflank Boxy
+// Copyright (C) 2019 Assured Information Security, Inc.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#include <bfgsl.h>
+#include <intrinsics.h>
+#include <bfvmm/memory_manager/memory_manager.h>
+#include <bfvmm/memory_manager/arch/x64/cr3.h>
+#include <bfvmm/hve/arch/x64/unmapper.h>
+#include <bfvmm/hve/arch/intel_x64/vtd/iommu.h>
+#include <hve/arch/intel_x64/iommu.h>
+
+using namespace bfvmm::x64;
+
+namespace boxy::intel_x64
+{
+
+iommu::iommu() noexcept : m_root{make_page<entry_t>()}
+{
+    /// Map in registers
+    ///
+    auto hva = g_mm->alloc_map(iommu::page_size);
+
+    g_cr3->map_4k(
+            hva,
+            iommu::hpa,
+            cr3::mmap::attr_type::read_write,
+            cr3::mmap::memory_type::uncacheable);
+
+    m_reg_map = bfvmm::x64::unique_map<uint8_t>(
+            static_cast<uint8_t *>(hva),
+            bfvmm::x64::unmapper(hva, iommu::page_size)
+    );
+}
+
+iommu *iommu::instance() noexcept
+{
+    static iommu self;
+    return &self;
+}
+
+void iommu::set_dom0_eptp(uintptr_t eptp)
+{ m_dom0_eptp = eptp; }
+
+void iommu::set_domU_eptp(uintptr_t eptp)
+{ m_domU_eptp = eptp; }
+
+void iommu::set_dom0_cte(iommu::entry_t *cte)
+{
+//    // TODO check TT against dev tlb support
+
+    cte->data[0] = m_dom0_eptp | 1U;  // present, points to dom0 ept
+    cte->data[1] = (1ULL << 8U) | 2U; // DID 1, 4-level EPT
+}
+
+void iommu::set_domU_cte(iommu::entry_t *cte)
+{
+//    // TODO check TT against dev tlb support
+
+    cte->data[0] = m_domU_eptp | 1U;  // present, points to domU ept
+    cte->data[1] = (2ULL << 8U) | 2U; // DID 2, 4-level EPT
+}
+
+static constexpr uint32_t devfn(uint32_t dev, uint32_t fn)
+{ return (dev << 3) | fn; }
+
+// Called once from {dom0, vcpu0}
+//
+void iommu::init_dom0_mappings()
+{
+    // Alloc ctxt table for each bus
+    m_ctxt.push_back(make_page<entry_t>());
+    m_ctxt.push_back(make_page<entry_t>());
+    m_ctxt.push_back(make_page<entry_t>());
+
+    // Setup root entries
+    entry_t *rte = m_root.get();
+    rte[0].data[0] = g_mm->virtptr_to_physint(m_ctxt[0].get()) | 1U;
+    rte[1].data[0] = g_mm->virtptr_to_physint(m_ctxt[1].get()) | 1U;
+
+    // Bus 0 for gigabyte board
+    entry_t *cte = m_ctxt[0].get();
+    this->set_dom0_cte(&cte[devfn(0x00, 0)]); // Host bridge
+    this->set_dom0_cte(&cte[devfn(0x02, 0)]); // VGA
+    this->set_dom0_cte(&cte[devfn(0x08, 0)]); // Gaussian model
+    this->set_dom0_cte(&cte[devfn(0x14, 0)]); // USB
+    this->set_dom0_cte(&cte[devfn(0x16, 0)]); // Comms
+    this->set_dom0_cte(&cte[devfn(0x17, 0)]); // SATA
+    this->set_dom0_cte(&cte[devfn(0x1B, 0)]); // PCI Bridge to bus 1
+    this->set_dom0_cte(&cte[devfn(0x1C, 0)]); // PCI Bridge to bus 2
+    this->set_dom0_cte(&cte[devfn(0x1C, 5)]); // PCI Bridge to bus 3
+    this->set_dom0_cte(&cte[devfn(0x1C, 6)]); // PCI Bridge to bus 4
+    this->set_dom0_cte(&cte[devfn(0x1C, 7)]); // PCI Bridge to bus 5
+    this->set_dom0_cte(&cte[devfn(0x1D, 0)]); // PCI Bridge to bus 6
+    this->set_dom0_cte(&cte[devfn(0x1D, 1)]); // PCI Bridge to bus 7
+    this->set_dom0_cte(&cte[devfn(0x1D, 2)]); // PCI Bridge to bus 8
+    this->set_dom0_cte(&cte[devfn(0x1D, 3)]); // PCI Bridge to bus 9
+    this->set_dom0_cte(&cte[devfn(0x1F, 0)]); // ISA bridge
+    this->set_dom0_cte(&cte[devfn(0x1F, 2)]); // Memory controller
+    this->set_dom0_cte(&cte[devfn(0x1F, 3)]); // Audio controller
+    this->set_dom0_cte(&cte[devfn(0x1F, 4)]); // SMBus controller
+
+    // Bus 1 for GB board
+    cte = m_ctxt[1].get();
+    this->set_dom0_cte(&cte[devfn(0x00, 0)]); // NVMe controller
+
+    ::x64::cache::wbinvd();
+}
+
+void iommu::init_domU_mappings()
+{
+    entry_t *rte = m_root.get();
+    rte[2].data[0] = g_mm->virtptr_to_physint(m_ctxt[2].get()) | 1U;
+
+    // Bus 2 for GB board
+    entry_t *cte = m_ctxt[2].get();
+    this->set_domU_cte(&cte[devfn(0x00, 0)]); // Ethernet controller
+
+    ::x64::cache::wbinvd();
+}
+
+void iommu::enable()
+{
+    expects(m_dom0_eptp != 0);
+    expects(m_domU_eptp != 0);
+
+    // Update the hva from the VMM's cr3.
+    // Any access to this member between now and
+    // VMX-root will fault.
+    //
+    m_hva = m_reg_map.get();
+
+//    auto cap = this->read64(0x8);
+    auto ecap = this->read64(0x10);
+    auto gsts = this->read32(0x1C);
+    auto rtar = this->read64(0x20);
+
+//    ::intel_x64::vtd::iommu::cap_reg::dump(0, cap);
+//    ::intel_x64::vtd::iommu::ecap_reg::dump(0, ecap);
+//    ::intel_x64::vtd::iommu::rtaddr_reg::dump(0, rtar);
+//    ::intel_x64::vtd::iommu::gsts_reg::dump(0, gsts);
+
+    expects(::intel_x64::vtd::iommu::gsts_reg::tes::is_disabled(gsts));
+    expects(::intel_x64::vtd::iommu::gsts_reg::qies::is_disabled(gsts));
+    expects(::intel_x64::vtd::iommu::gsts_reg::ires::is_disabled(gsts));
+    expects(::intel_x64::vtd::iommu::rtaddr_reg::ttm::get(rtar) == 0);
+
+    //
+    // Set the root address with legacy translation mode
+    //
+
+    this->write64(0x20, g_mm->virtptr_to_physint(m_root.get()));
+    ::intel_x64::barrier::mb();
+    gsts = this->read32(0x1C);
+    uint32_t gcmd = (gsts & 0x96FFFFFFU) | (1UL << 30);
+    this->write32(0x18, gcmd);
+
+    ::intel_x64::barrier::mb();
+    while ((this->read32(0x1C) | (1UL << 30)) == 0) {
+        ::intel_x64::pause();
+    }
+
+    //
+    // Once the RTAR is set, the context-cache and IOTLB must be invalidated
+    //
+
+    uint64_t ctxcmd = this->read64(0x28);
+    expects((ctxcmd & 0x8000000000000000U) == 0);
+    ctxcmd &= ~0x6000000000000000U;
+    ctxcmd |= (1ULL << 61); // global invalidation
+    ctxcmd |= (1ULL << 63); // do it
+    this->write64(0x28, ctxcmd);
+
+    ::intel_x64::barrier::mb();
+    while ((this->read64(0x28) & (1ULL << 63)) != 0) {
+        ::intel_x64::pause();
+    }
+
+    uint64_t iva = ::intel_x64::vtd::iommu::ecap_reg::iro::get(ecap);
+    expects(iva == 0x50);
+    uint64_t iotlb = this->read64(iva + 0x8);
+    iotlb &= ~0x3000000000000000U;
+    iotlb |= (1ULL << 60); // global invalidation
+    iotlb |= (1ULL << 63); // do it
+    this->write64(iva + 0x8, iotlb);
+
+    ::intel_x64::barrier::mb();
+    while ((this->read64(iva + 0x8) & (1ULL << 63)) != 0) {
+        ::intel_x64::pause();
+    }
+
+    //
+    // Enable DMA remapping
+    //
+
+    gsts = this->read32(0x1C);
+    gcmd = (gsts & 0x96FFFFFFU) | (1UL << 31);
+    this->write32(0x18, gcmd);
+
+    ::intel_x64::barrier::mb();
+    while ((this->read32(0x1C) | (1UL << 31)) == 0) {
+        ::intel_x64::pause();
+    }
+
+    bfdebug_info(0, "DMA remapping enabled");
+}
+
+void iommu::disable()
+{
+    uint32_t gsts = this->read32(0x1C);
+    uint32_t gcmd = (gsts & 0x96FFFFFFU) | (0UL << 31);
+    this->write32(0x18, gcmd);
+
+    ::intel_x64::barrier::mb();
+    while ((this->read32(0x1C) | (1UL << 31)) != 0) {
+        ::intel_x64::pause();
+    }
+}
+
+
+/// Register access
+///
+uint64_t iommu::read64(uintptr_t off)
+{
+    uint64_t *addr = reinterpret_cast<uint64_t *>(m_hva + off);
+    return *addr;
+}
+
+uint32_t iommu::read32(uintptr_t off)
+{
+    uint32_t *addr = reinterpret_cast<uint32_t *>(m_hva + off);
+    return *addr;
+}
+
+void iommu::write64(uintptr_t off, uint64_t val)
+{
+    uint64_t *addr = reinterpret_cast<uint64_t *>(m_hva + off);
+    *addr = val;
+}
+
+void iommu::write32(uintptr_t off, uint64_t val)
+{
+    uint32_t *addr = reinterpret_cast<uint32_t *>(m_hva + off);
+    *addr = gsl::narrow_cast<uint32_t>(val);
+}
+
+}
