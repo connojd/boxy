b2ac43814cd0f70b9f35c22cdfca4cf62c3345a1
Connor Davis <davisc@ainfosec.com>
2019-03-22 22:11:12 -0600

    Add struct pci_dev
    Introduce a structure to represent devices intended
    for PCI passthrough. Each struct pci_dev has a physical
    and virtual vector number and a vcpuid in addition to
    the standard bus/device/function.
    
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

---
+++ bfvmm/include/hve/arch/intel_x64/pci/pci_dev.h
@@ -0,0 +1,171 @@
+//
+// Copyright (C) 2019 Assured Information Security, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef PCI_DEV_INTEL_X64_BOXY_H
+#define PCI_DEV_INTEL_X64_BOXY_H
+
+#include <bfhypercall.h>
+#include <bfvmm/hve/arch/intel_x64/vcpu.h>
+#include <bfvmm/hve/arch/intel_x64/vmexit/io_instruction.h>
+
+// -----------------------------------------------------------------------------
+// Definitions
+// -----------------------------------------------------------------------------
+
+namespace boxy::intel_x64
+{
+
+class vcpu;
+
+class pci_dev
+{
+public:
+
+    /// Constructor
+    ///
+    /// @expects
+    /// @ensures
+    ///
+    explicit pci_dev(uint32_t bus, uint32_t dev, uint32_t fun);
+
+    /// Destructor
+    ///
+    /// @expects
+    /// @ensures
+    ///
+    ~pci_dev() = default;
+
+    /// Bind vcpu
+    ///
+    /// Associate the vcpu referenced by the given id with this PCI dev.
+    ///
+    /// @param id the vcpuid to bind
+    ///
+    void bind_vcpu(vcpuid_t id);
+
+    /// Set physical vector
+    ///
+    /// @param phys the vector received from the visr driver
+    ///
+    void set_phys_vector(uint64_t phys);
+
+    /// Set virtual vector
+    ///
+    /// @param virt the vector received from the guest VM
+    ///
+    void set_virt_vector(uint64_t virt);
+
+    /// Set bus/device/function
+    ///
+    /// @param bus the bus of this device
+    /// @param dev the device of this device
+    /// @param fun the function of this device
+    ///
+    void set_bdf(uint32_t bus, uint32_t dev, uint32_t fun);
+
+    /// Set register value
+    ///
+    /// @param off the offset of the register to set
+    /// @param val the value to set it to
+    ///
+    void set_reg(uint32_t off, uint32_t val);
+
+    /// Get bus
+    ///
+    /// @return the bus of this device
+    ///
+    uint32_t bus() const;
+
+    /// Get dev
+    ///
+    /// @return the dev of this device
+    ///
+    uint32_t dev() const;
+
+    /// Get function
+    ///
+    /// @return the function of this device
+    ///
+    uint32_t fun() const;
+
+    /// Get register
+    ///
+    /// @param offset the register offset to read
+    /// @return the value of the register at offset
+    ///
+    uint32_t reg(uint32_t offset) const;
+
+    /// Physical vector
+    ///
+    /// @return the physical vector of this device
+    ///
+    uint64_t phys_vector() const;
+
+    /// Virtual vector
+    ///
+    /// @return the virtual vector of this device
+    ///
+    uint64_t virt_vector() const;
+
+    /// vcpuid
+    ///
+    /// @return the vcpuid bound to this device
+    ///
+    uint64_t vcpuid() const;
+
+    /// Bound to vcpu
+    ///
+    /// @param id the vcpu to check
+    /// @return true iff this is bound to the vcpu referenced by the id
+    ///
+    bool bound_to_vcpu(vcpuid_t id) const;
+
+private:
+
+    uint32_t m_bus{};
+    uint32_t m_dev{};
+    uint32_t m_fun{};
+    uint32_t m_phys_vector{};
+    uint32_t m_virt_vector{};
+    vcpuid_t m_vcpuid{INVALID_VCPUID};
+    domainid_t m_domid{INVALID_DOMAINID};
+
+    // Original PCI has 256 bytes of configuration space
+    static constexpr auto cfg_size = 256 / sizeof(uint32_t);
+    std::unique_ptr<uint32_t[]> m_cfg_ptr{};
+    gsl::span<uint32_t> m_cfg{};
+
+public:
+
+    /// @cond
+
+    pci_dev(pci_dev &&) = default;
+    pci_dev &operator=(pci_dev &&) = default;
+
+    pci_dev(const pci_dev &) = delete;
+    pci_dev &operator=(const pci_dev &) = delete;
+
+    /// @endcond
+};
+
+}
+
+#endif
---
+++ bfvmm/src/hve/arch/intel_x64/pci/pci_dev.cpp
@@ -0,0 +1,88 @@
+//
+// Copyright (C) 2019 Assured Information Security, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <hve/arch/intel_x64/vcpu.h>
+#include <hve/arch/intel_x64/pci/pci_dev.h>
+
+// -----------------------------------------------------------------------------
+// Implementation
+// -----------------------------------------------------------------------------
+
+namespace boxy::intel_x64
+{
+
+pci_dev::pci_dev(uint32_t bus, uint32_t dev, uint32_t fun) :
+    m_bus{bus},
+    m_dev{dev},
+    m_fun{fun},
+    m_cfg_ptr{std::make_unique<uint32_t[]>(cfg_size)},
+    m_cfg{gsl::span<uint32_t>(m_cfg_ptr.get(), cfg_size)}
+{ }
+
+uint32_t pci_dev::bus() const { return m_bus; }
+uint32_t pci_dev::dev() const { return m_dev; }
+uint32_t pci_dev::fun() const { return m_fun; }
+uint32_t pci_dev::reg(uint32_t off) const { return m_cfg.at(off); }
+vcpuid_t pci_dev::vcpuid() const { return m_vcpuid; }
+
+void pci_dev::set_reg(uint32_t off, uint32_t val)
+{
+    m_cfg.at(off) = val;
+}
+
+void pci_dev::bind_vcpu(vcpuid_t id)
+{
+    m_vcpuid = id;
+}
+
+bool pci_dev::bound_to_vcpu(vcpuid_t id) const
+{
+    return id == m_vcpuid;
+}
+
+void pci_dev::set_phys_vector(uint64_t phys)
+{
+    m_phys_vector = phys;
+}
+
+void pci_dev::set_virt_vector(uint64_t virt)
+{
+    m_virt_vector = virt;
+}
+
+uint64_t pci_dev::phys_vector() const
+{
+    return m_phys_vector;
+}
+
+uint64_t pci_dev::virt_vector() const
+{
+    return m_virt_vector;
+}
+
+void pci_dev::set_bdf(uint32_t bus, uint32_t dev, uint32_t fun)
+{
+    m_bus = bus;
+    m_dev = dev;
+    m_fun = fun;
+}
+
+}
